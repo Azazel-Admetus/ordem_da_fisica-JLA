c9e7d38daa4adfdccc643e5d2bfb60c6ee024a6d
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-02-21 10:37:27 -0400

    Fiz a lógica com o banco de dados para o recrutamento

---
+++ php/formulario.php
@@ -0,0 +1,28 @@
+<?php
+require_once "conn.php";
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $codigo_de_acesso = $_POST['codigo'];
+    $arquivo = 'codigo.csv';
+    if(($handle = fopen($arquivo, "r")) !== FALSE ){
+        $dado =trim( fgets($handle));
+            fclose($handle);
+            if($codigo_de_acesso == $dado){
+                $nome = $_POST['nome'];
+                $email = $_POST['email'];
+                $turno = $_POST['turno'];
+                $turma = $_POST['turma'];
+                if(!empty($nome) && !empty($email) && !empty($turno) && !empty($turma)){
+                    $stmt = $conn->prepare("INSERT INTO recrutamento (nome, email, turno, turma) VALUES (:nome, :email, :turno, :turma)");
+                    $stmt->bindParam(':nome', $nome);
+                    $stmt->bindParam(':email', $email);
+                    $stmt->bindParam(':turno', $turno);
+                    $stmt->bindParam(':turma', $turma);
+                    if($stmt->execute()){
+                        header('Location:../html/formulario.html?answer=True');
+                        exit;
+                    }
+                }
+            }
+
+        }
+    }
