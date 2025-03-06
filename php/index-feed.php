c866045f9d527c08688fc410919c39b4e60783f1
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-03-06 11:40:28 -0400

    Fiz a página do feed, consegui exibir as informações dos posts sem problemas. Agora falta apenas estilizar todo o site. Expectativa é de término até o meio da semana que vem

---
+++ php/index-feed.php
@@ -0,0 +1,71 @@
+<?php
+require_once "conn.php";
+session_start();//inicia a sessão
+//pegando o conteúdo do db
+$stmt = $conn->prepare("SELECT titulo, conteudo, imagem, tipo_mime, admin_id, criado_data from post where admin_id = :id_user");
+$id_user = $_SESSION['user_id'];
+$stmt->bindValue(':id_user', $id_user);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//verificando se há resultados
+if(!$posts){
+   echo "
+   <h5 id='error-msg'>Nenhum conteúdo encontrado.</h5>";
+   exit;
+}
+?>
+<!-- inserindo na página html -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Clube de Física - JLA</title>
+</head>
+<body>
+    <main>
+        <header>
+            <h1>Ordem da Física</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="../html/login.html">Login</a>
+                    </li>
+                    <li>
+                        <a href="../html/cadastro.html">Cadastre-se</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <a href="../html/feed_create.html">crie publicações</a>
+        </section>
+        <?php foreach ($posts as $post): ?>
+            <?php
+            //extraindo os dados 
+                $titulo = $post['titulo'] ? htmlspecialchars($post['titulo']) : "Sem título";
+                $conteudo = nl2br(htmlspecialchars($post['conteudo']));
+                $imagem = $post['imagem'];
+                $tipo_mime = $post['tipo_mime'] ?? 'image/jpeg' ;
+                $criado_data = date('d/m/Y', strtotime($post['criado_data']));
+                $criado_hora = date('H:i', strtotime($post['criado_data']));
+                ?>
+                <section>
+                    <?php if(!empty($post['titulo'])): ?>
+                        <h3><?php echo $titulo; ?></h3>
+                    <?php endif; ?>
+                    <p><?= $conteudo; ?></p>
+                    <?php if(!empty($imagem)): ?>
+                        <img
+                            src="data:<?php echo $tipo_mime; ?>;base64,<?php echo base64_encode($imagem);?>"
+                            alt="Imagem da publicação feita pelo admin ao blog"
+                        >
+                    <?php endif; ?>
+                    <small>Publicado em: <?php echo $criado_data; ?> às <?php echo $criado_hora;?></small>
+                </section>
+         <?php endforeach; ?>
+        <footer></footer>
+    </main>
+</body>
+</html>
