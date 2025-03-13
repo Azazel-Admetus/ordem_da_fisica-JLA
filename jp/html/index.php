9e8b4332808caa2c7a9d93e7fc2377f12acfb522
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-03-13 12:05:11 -0400

    Falta apenas melhorar o footer, traduzir as páginas e adicionar mais segurança

--- jp/html/index.php
+++ jp/html/index.php
@@ -6,7 +6,7 @@ if(isset($_GET['idioma'])){
 }
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="jp">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
