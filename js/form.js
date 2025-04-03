7b0b2a07894c976c67440770dd3f90519895d834
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-04-03 08:28:30 -0400

    bug do btn

--- js/form.js
+++ js/form.js
@@ -1,9 +1,11 @@
 document.getElementById("matutino").addEventListener("click", () => {
+    console.log("matutino clicado");
     document.getElementById("turma_matutino").style.display = "block";
     document.getElementById("turma_vespertino").style.display = "none";
 });
 
 document.getElementById("vespertino").addEventListener("click", () => {
+    console.log("vespertino clicado");
     document.getElementById("turma_vespertino").style.display = "block";
     document.getElementById("turma_matutino").style.display = "none";
 });
