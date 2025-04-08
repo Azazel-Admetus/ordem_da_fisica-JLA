e3186106c76e669576b22607b701ae989cc6403f
Gustavo de Souza Vieira <gustavo@CamOPO2.com>
2025-04-08 10:45:11 -0400

    adicionando tratamento de erros ao arquivo para fazer a verificação

--- php/post.php
+++ php/post.php
@@ -1,5 +1,8 @@
 <?php
 require_once "conn.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SESSION['user_id']) && $_SESSION['user_tipo'] == 'admin' ){
     $titulo = isset($_POST['titulo']) ? htmlspecialchars(trim(($_POST['titulo']))) : null;
