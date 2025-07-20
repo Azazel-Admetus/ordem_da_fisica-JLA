8a0506ea1c7f6b66d3def521bac789180b288400
azazel_admetus <gustavodsv7184@gmail.com>
2025-07-19 20:36:11 -0400

    Removendo as pastas do site em outro idioma

--- jp/js/form.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("matutino").addEventListener("click", () => {
-    document.getElementById("turma_matutino").style.display = "block";
-    document.getElementById("turma_vespertino").style.display = "none";
-});
-
-document.getElementById("vespertino").addEventListener("click", () => {
-    document.getElementById("turma_vespertino").style.display = "block";
-    document.getElementById("turma_matutino").style.display = "none";
-});
