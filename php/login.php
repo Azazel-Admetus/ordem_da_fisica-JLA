e041a4ff06246c13982f793692a086562180a15c
azazel_admetus <gustavodsv7184@gmail.com>
2025-07-20 20:33:20 -0400

    Fiz o tratamento de erros na página login.html, movi o index.html para fora das pastas porque é assim que ele fica lá na hospedagem, e mudei algumas coisas no login.php, como adicionar mais uma sanitização

--- php/login.php
+++ php/login.php
@@ -1,29 +1,29 @@
 <?php
-require_once "conn.php";
+require_once 'conn.php';
 session_start();
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
-    $senha = trim($_POST['senha']);
-    if(!empty($email) && !empty($senha)){
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = trim(strip_tags($_POST['email']));
+    $senha = trim(strip_tags($_POST['senha']));
+    if(!empty($email) && !empty($senha) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $stmt = $conn->prepare('SELECT * FROM usuarios WHERE email = :email');
+        $stmt->bindParam(':email', $email);
         $stmt->execute();
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        //prevenção contra ataques de tempo
-        $hash = $user['senha'] ?? password_hash("placeholder_password", PASSWORD_DEFAULT);
-        if ($user && password_verify($senha, $hash)){
-            session_regenerate_id(true); //evita sequestro de sessão
+        if ($user && password_verify($senha, $user['senha'])) {
+            session_regenerate_id(true);
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['nome'];
-            $_SESSION['user_tipo'] = $user['tipo'];
-            header('Location:index-pos.php');
-            exit;
-        }else{
-            header('Location:../html/login.php?error=invalid_credentials');
-            exit;
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['tipo_usuario'] = $user['tipo_usuario'];
+            header("Location:redirecionador.php?fluxo=login");
+            exit();
+        } else {
+            header("Location:../html/login.html?error=credenciais_invalidas");
+            exit();
         }
-    }else{
-        header('Location:../html/login.php?empty=true');
-        exit;
+    } else {
+        header("Location:../html/login.html?error=campos_vazios");
+        exit();
     }
 }
+?>
