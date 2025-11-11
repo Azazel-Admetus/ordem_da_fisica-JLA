7cd9937adba5fe924c829c6b75591cf7ef0a75cb
azazel_admetus <gustavodsv7184@gmail.com>
2025-11-11 09:16:38 -0400

    Fiz a verificação dos códigos php e deu tudo certo. Corrigi sem problemas os problemas que ocorreram

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,18 +1,25 @@
 <?php 
 require_once 'conn.php';
+require_once 'error_log.php';
 session_start();
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     //pegando os valores do formulário e sanitizando
-    $nome = trim(mb_substr(strip_tags($_POST['nome']), 0, 100)); 
-    $email = filter_var(trim(mb_substr(strip_tags($_POST['email']), 0, 100)), FILTER_VALIDATE_EMAIL);
+    $nome = trim(strip_tags($_POST['nome']));
+    $nome = mb_substr($nome, 0, 100);
+
+    $email = trim(strip_tags($_POST['email']));
+    $email = mb_substr($email, 0, 100);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    $senhaBruta = trim($_POST['senha']);
     //verifico se a senha tem mais de 8 caracteres
     if(strlen($_POST['senha']) < 8){
         header('Location:../html/cadastro_usuarios.html?error=sizeSlow');
         exit;
     }
     //verifica se estão vazios e se o email é válido, e sim, eu fiz a verificação do email novamente
-    if(!empty($nome) && !empty($email) && !empty($senha) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+    if(!empty($nome) && !empty($email) && !empty($senhaBruta) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $senha = password_hash($senhaBruta, PASSWORD_DEFAULT);
         $stmt = $conn->prepare('SELECT COUNT(*) FROM usuarios WHERE nome = :nome OR email = :email'); //verifica se o usuário já existe
         $stmt->bindParam(':nome', $nome);
         $stmt->bindParam(':email', $email);
